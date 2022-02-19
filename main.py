d4c1e78de37ce00b47d489532893895bfc6b3c94
Geoffrey <g.h.garrett13@gmail.com>
2022-02-19 15:50:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+
+def main():
+    my_input = os.environ["INPUT_MYINPUT"]
+
+    my_output = f"Hello {my_input}"
+
+    print(f"::set-output name=myOutput::{my_output}")
+
+
+if __name__ == "__main__":
+    main()
