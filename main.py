5ed4914688fa4e6a7f7ed64f9c9612761ee08a1c
Geoffrey <g.h.garrett13@gmail.com>
2022-02-21 13:07:09 +0200

    Updated for templating

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ import logging
 import pprint
 from github import Github
 import pygit2
+import re
 
 LOGGER = logging.getLogger(__name__)
+
+
+def clone_repo(clone_url, clone_path, branch, auth_token):
+    # Use pygit2 to clone the repo to disk
+    # if using github app pem key token, use x-access-token like below
+    # if you were using a personal access token, use auth_method = 'x-oauth-basic' AND reverse the auth_method and token parameters
+    auth_method = 'x-access-token'
+    callbacks = pygit2.RemoteCallbacks(pygit2.UserPass(auth_method, auth_token))
+    pygit2_repo = pygit2.clone_repository(clone_url, clone_path,
+                                          callbacks=callbacks)
+    pygit2_branch = pygit2_repo.branches['origin/' + branch]
+    pygit2_ref = pygit2_repo.lookup_reference(pygit2_branch.name)
+    pygit2_repo.checkout(pygit2_ref)
+    return pygit2_repo
+
 
 def main():
     with open(os.environ["GITHUB_EVENT_PATH"], 'r') as fp:
         event_data = json.load(fp)
     event_name = os.environ['GITHUB_EVENT_NAME'].lower()
-
-    github = Github(os.environ['GH_TOKEN'])
-    github.get_repo(event_data['repository'])
 
     LOGGER.info('github event: %s', event_name)
     LOGGER.info('github event data:\n%s', pprint.pformat(event_data))
@@ -21,28 +34,136 @@ def main():
     if event_name == 'repository_dispatch':
         LOGGER.info('repository_dispatch event')
 
-        # get feedstock of repository
-        repo = github.get_repo(event_data['repository']+"-feedstock")
+        gh = Github(os.environ['GH_TOKEN'])
+
+        # get repositories of project and respective feedstock
+        project_repo = gh.get_repo(event_data['repository'])
+        feedstock_repo = gh.get_repo(event_data['repository'] + "-feedstock")
 
         # check if repo exists
-        if repo is None:
-            LOGGER.info('repository_dispatch event: feedstock repository not found')
+        if feedstock_repo is None:
+            LOGGER.info(
+                'repository_dispatch event: feedstock repository not found')
             return
 
-        ref_name = event_data['ref_name']
-        cloned_repo = pygit2.clone_repository(repo.git_url, './tmp/feedstock')
-        branch = cloned_repo.lookup_branch(ref_name)
-        ref = cloned_repo.lookup_reference(branch.name)
-        cloned_repo.checkout(ref)
+        if event_data["ref_type"] != "branch":
+            LOGGER.info(
+                'repository_dispatch event: only branch ref type supported'
+            )
+            return
+
+        branch = event_data["ref_name"]
+        contents = project_repo.get_contents("version", ref=branch)
+        version = contents.decoded_content.decode("ascii")
+
+        # remap branch to subchannel
+        subchannel = remap(event_data["ref_name"])
+
+        # clone feedstock and checkout template branch
+        cloned_repo = clone_repo(
+            feedstock_repo.clone_url,
+            os.path.join(os.environ['GITHUB_WORKSPACE'], "feedstock"),
+            'template',
+            os.environ['GH_TOKEN'])
+
+        previous_metadata = feedstock_repo.get_contents(
+            "recipe/meta.yaml", ref=branch).decode("ascii")
+        # use regex to get last version {% set version = "@VERSION@" %}
+        previous_version = re.search(
+            r'{%\s*set\s*version\s*=\s*"([^"]*)"\s*%}',
+            previous_metadata).group(1)
+        previous_build = re.search(
+            r'{%\s*set\s*build\s*=\s*"([^"]*)"\s*%}',
+            previous_metadata).group(1)
+        previous_git_rev = re.search(
+            r'{%\s*set\s*git_rev\s*=\s*"([^"]*)"\s*%}',
+            previous_metadata).group(1)
+
+        # if versions are the same, increment build
+        if version == previous_version:
+            new_build = int(previous_build) + 1
+        else:
+            new_build = 1
+
+        vars = {
+            "SUBCHANNEL": subchannel,
+            "REPO_NAME": event_data["repository"],
+            "VERSION": version,
+            "BUILD": new_build,
+            "GIT_REV": event_data["sha"],
+        }
+
+        # for each file, replace @VAR@ with values from dictionary
+        for root, dirs, files in os.walk(
+                os.path.join(os.environ['GITHUB_WORKSPACE'], "feedstock")):
+            for file in files:
+                with open(os.path.join(root, file), 'r') as f:
+                    file_contents = f.read()
+
+                for key, value in vars.items():
+                    file_contents = file_contents.replace(
+                        "@" + key + "@", value)
+
+                with open(os.path.join(root, file), 'w') as f:
+                    f.write(file_contents)
+
+        user = "Delfi-C3"
+        email = "Delfi-C3@users.noreply.github.com"
+
+        # commit feedstock changes
+        cloned_repo.index.add_all()
+        cloned_repo.index.write()
+        author = pygit2.Signature(user, email)
+        commiter = pygit2.Signature(user, email)
+        tree = cloned_repo.index.write_tree()
+        pygit2_branch = cloned_repo.branches['origin/' + branch]
+        pygit2_ref = cloned_repo.lookup_reference(pygit2_branch.name)
+        oid = cloned_repo.create_commit(pygit2_ref, author, commiter,
+                                        f"BOT: Automated feedstock update for sha:{event_data['sha']} on {event_data['repository']}",
+                                        tree,
+                                        [cloned_repo.head.get_object().hex])
+
+        auth_method = 'x-access-token'
+        credentials = pygit2.UserPass(auth_method, os.environ['GH_TOKEN'])
+        callbacks = pygit2.RemoteCallbacks(
+            credentials=credentials
+        )
+        remote = cloned_repo.remotes["origin"]
+        remote.push([pygit2_ref], callbacks=callbacks)
+
+
+def remap(key):
+    key = {
+        "develop": "dev",
+        "master": "main",
+        "main": "main",
+    }
+    if key in remap:
+        return remap[key]
+    return key
+
 
 if __name__ == "__main__":
-    github = Github('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')
-    repo = github.get_repo('tudat-team/tudat-resources-feedstock')
+    main()
+    # token = ''
+    # github = Github(token)
+    # repo = github.get_repo('tudat-team/tudat-resources-feedstock')
+    # project_repo = github.get_repo('tudat-team/tudat-resources')
+    # fil = project_repo.get_contents("version")
+    # # print(fil.content)
+    #
+    # # repo = g.get_repo("PyGithub/PyGithub")
+    #
+    # branch = "develop"
+    # contents = project_repo.get_contents("version", ref=branch)
+    # version = contents.decoded_content.decode("ascii")
 
-    cloned_repo = pygit2.clone_repository(repo.git_url, './here')
-    branch = cloned_repo.lookup_branch('template')
-    ref = cloned_repo.lookup_reference(branch.name)
-    cloned_repo.checkout(ref)
+    # cloned_repo = clone_repo(repo..git_url, './tmp/feedstock', branch, token)
 
-    # print(repo.last_modified)
+    # vars = {
+    #     "REPOSITORY": "tudat-team/tudat-resources-feedstock",
+    #
+    #     "SUBCHANNEL": branch_channel_map(branch)
+    # }
+
     # main()
